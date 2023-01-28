6df8853b563dbf9a9eb30dcf41aa3b0071e02be0
lby132 <quddbscjswow@naver.com>
2023-01-28 23:58:18 +0900

    js

---
+++ app.js
@@ -0,0 +1,17 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+const link = document.querySelector("a");
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(loginInput);
+}
+
+function handleLinkClick(event) {
+    event.preventDefault();
+    alert("clciked")
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);

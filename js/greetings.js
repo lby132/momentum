aa65d426b4ccc3864f2f386ee9571caec3bafc20
lby132 <quddbscjswow@naver.com>
2023-01-29 23:43:08 +0900

    js

--- js/greetings.js
+++ js/greetings.js
@@ -3,19 +3,28 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username) {
     greeting.innerText = `Hello  ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-function handleLinkClick(event) {
-    event.preventDefault();
-    alert("clicked")
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    paintGreetings(savedUsername);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
-// link.addEventListener("click", handleLinkClick);
